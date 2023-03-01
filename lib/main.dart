a8a19ba43a127ffd8cef15990fbf2bac0b2c4f20
Matthieu Gascon <matthieu.gascon@hotmail.fr>
2023-03-01 13:28:36 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
+/*
 import 'dart:async';
 
 import 'package:firebase_analytics/firebase_analytics.dart';
@@ -11,366 +11,179 @@ import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
 import 'tabs_page.dart';
+*/
+//source:
+//suivi la video https://www.youtube.com/watch?v=aKgEEnVhU1I&t=61s
+//positionné les éléments: https://medium.flutterdevs.com/stack-and-positioned-widget-in-flutter-3d1a7b30b09a
+import 'package:flutter/material.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
-  static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
-  static FirebaseAnalyticsObserver observer =
-      FirebaseAnalyticsObserver(analytics: analytics);
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Firebase Analytics Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      navigatorObservers: <NavigatorObserver>[observer],
-      home: MyHomePage(
-        title: 'Firebase Analytics Demo',
-        analytics: analytics,
-        observer: observer,
-      ),
+      home: ConnectPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({
-    Key? key,
-    required this.title,
-    required this.analytics,
-    required this.observer,
-  }) : super(key: key);
-
-  final String title;
-  final FirebaseAnalytics analytics;
-  final FirebaseAnalyticsObserver observer;
+class PageAccueil extends StatefulWidget {
+  const PageAccueil({super.key});
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<PageAccueil> createState() => _PageAccueilState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  String _message = '';
-
-  void setMessage(String message) {
-    setState(() {
-      _message = message;
-    });
+class _PageAccueilState extends State<PageAccueil> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
+}
 
-  Future<void> _setDefaultEventParameters() async {
-    if (kIsWeb) {
-      setMessage(
-        '"setDefaultEventParameters()" is not supported on web platform',
-      );
-    } else {
-      // Only strings, numbers & null (longs & doubles for android, ints and doubles for iOS) are supported for default event parameters:
-      await widget.analytics.setDefaultEventParameters(<String, dynamic>{
-        'string': 'string',
-        'int': 42,
-        'long': 12345678910,
-        'double': 42.0,
-        'bool': true.toString(),
-      });
-      setMessage('setDefaultEventParameters succeeded');
-    }
-  }
+class ConnectPage extends StatefulWidget {
+  const ConnectPage({super.key});
 
-  Future<void> _sendAnalyticsEvent() async {
-    // Only strings and numbers (longs & doubles for android, ints and doubles for iOS) are supported for GA custom event parameters:
-    // https://firebase.google.com/docs/reference/ios/firebaseanalytics/api/reference/Classes/FIRAnalytics#+logeventwithname:parameters:
-    // https://firebase.google.com/docs/reference/android/com/google/firebase/analytics/FirebaseAnalytics#public-void-logevent-string-name,-bundle-params
-    await widget.analytics.logEvent(
-      name: 'test_event',
-      parameters: <String, dynamic>{
-        'string': 'string',
-        'int': 42,
-        'long': 12345678910,
-        'double': 42.0,
-        // Only strings and numbers (ints & doubles) are supported for GA custom event parameters:
-        // https://developers.google.com/analytics/devguides/collection/analyticsjs/custom-dims-mets#overview
-        'bool': true.toString(),
-      },
-    );
+  @override
+  State<ConnectPage> createState() => _ConnectPageState();
+}
 
-    setMessage('logEvent succeeded');
-  }
-
-  Future<void> _testSetUserId() async {
-    await widget.analytics.setUserId(id: 'some-user');
-    setMessage('setUserId succeeded');
-  }
-
-  Future<void> _testSetCurrentScreen() async {
-    await widget.analytics.setCurrentScreen(
-      screenName: 'Analytics Demo',
-      screenClassOverride: 'AnalyticsDemo',
-    );
-    setMessage('setCurrentScreen succeeded');
-  }
-
-  Future<void> _testSetAnalyticsCollectionEnabled() async {
-    await widget.analytics.setAnalyticsCollectionEnabled(false);
-    await widget.analytics.setAnalyticsCollectionEnabled(true);
-    setMessage('setAnalyticsCollectionEnabled succeeded');
-  }
-
-  Future<void> _testSetSessionTimeoutDuration() async {
-    await widget.analytics
-        .setSessionTimeoutDuration(const Duration(milliseconds: 20000));
-    setMessage('setSessionTimeoutDuration succeeded');
-  }
-
-  Future<void> _testSetUserProperty() async {
-    await widget.analytics.setUserProperty(name: 'regular', value: 'indeed');
-    setMessage('setUserProperty succeeded');
-  }
-
-  Future<void> _testAppInstanceId() async {
-    String? id = await widget.analytics.appInstanceId;
-    if (id != null) {
-      setMessage('appInstanceId succeeded: $id');
-    } else {
-      setMessage('appInstanceId failed, consent declined');
-    }
-  }
-
-  Future<void> _testResetAnalyticsData() async {
-    await widget.analytics.resetAnalyticsData();
-    setMessage('resetAnalyticsData succeeded');
-  }
-
-  AnalyticsEventItem itemCreator() {
-    return AnalyticsEventItem(
-      affiliation: 'affil',
-      coupon: 'coup',
-      creativeName: 'creativeName',
-      creativeSlot: 'creativeSlot',
-      discount: 2.22,
-      index: 3,
-      itemBrand: 'itemBrand',
-      itemCategory: 'itemCategory',
-      itemCategory2: 'itemCategory2',
-      itemCategory3: 'itemCategory3',
-      itemCategory4: 'itemCategory4',
-      itemCategory5: 'itemCategory5',
-      itemId: 'itemId',
-      itemListId: 'itemListId',
-      itemListName: 'itemListName',
-      itemName: 'itemName',
-      itemVariant: 'itemVariant',
-      locationId: 'locationId',
-      price: 9.99,
-      currency: 'USD',
-      promotionId: 'promotionId',
-      promotionName: 'promotionName',
-      quantity: 1,
-    );
-  }
-
-  Future<void> _testAllEventTypes() async {
-    await widget.analytics.logAddPaymentInfo();
-    await widget.analytics.logAddToCart(
-      currency: 'USD',
-      value: 123,
-      items: [itemCreator(), itemCreator()],
-    );
-    await widget.analytics.logAddToWishlist();
-    await widget.analytics.logAppOpen();
-    await widget.analytics.logBeginCheckout(
-      value: 123,
-      currency: 'USD',
-      items: [itemCreator(), itemCreator()],
-    );
-    await widget.analytics.logCampaignDetails(
-      source: 'source',
-      medium: 'medium',
-      campaign: 'campaign',
-      term: 'term',
-      content: 'content',
-      aclid: 'aclid',
-      cp1: 'cp1',
-    );
-    await widget.analytics.logEarnVirtualCurrency(
-      virtualCurrencyName: 'bitcoin',
-      value: 345.66,
-    );
-
-    await widget.analytics.logGenerateLead(
-      currency: 'USD',
-      value: 123.45,
-    );
-    await widget.analytics.logJoinGroup(
-      groupId: 'test group id',
-    );
-    await widget.analytics.logLevelUp(
-      level: 5,
-      character: 'witch doctor',
-    );
-    await widget.analytics.logLogin(loginMethod: 'login');
-    await widget.analytics.logPostScore(
-      score: 1000000,
-      level: 70,
-      character: 'tiefling cleric',
-    );
-    await widget.analytics
-        .logPurchase(currency: 'USD', transactionId: 'transaction-id');
-    await widget.analytics.logSearch(
-      searchTerm: 'hotel',
-      numberOfNights: 2,
-      numberOfRooms: 1,
-      numberOfPassengers: 3,
-      origin: 'test origin',
-      destination: 'test destination',
-      startDate: '2015-09-14',
-      endDate: '2015-09-16',
-      travelClass: 'test travel class',
-    );
-    await widget.analytics.logSelectContent(
-      contentType: 'test content type',
-      itemId: 'test item id',
-    );
-    await widget.analytics.logSelectPromotion(
-      creativeName: 'promotion name',
-      creativeSlot: 'promotion slot',
-      items: [itemCreator()],
-      locationId: 'United States',
-    );
-    await widget.analytics.logSelectItem(
-      items: [itemCreator(), itemCreator()],
-      itemListName: 't-shirt',
-      itemListId: '1234',
-    );
-    await widget.analytics.logScreenView(
-      screenName: 'tabs-page',
-    );
-    await widget.analytics.logViewCart(
-      currency: 'USD',
-      value: 123,
-      items: [itemCreator(), itemCreator()],
-    );
-    await widget.analytics.logShare(
-      contentType: 'test content type',
-      itemId: 'test item id',
-      method: 'facebook',
-    );
-    await widget.analytics.logSignUp(
-      signUpMethod: 'test sign up method',
-    );
-    await widget.analytics.logSpendVirtualCurrency(
-      itemName: 'test item name',
-      virtualCurrencyName: 'bitcoin',
-      value: 34,
-    );
-    await widget.analytics.logViewPromotion(
-      creativeName: 'promotion name',
-      creativeSlot: 'promotion slot',
-      items: [itemCreator()],
-      locationId: 'United States',
-      promotionId: '1234',
-      promotionName: 'big sale',
-    );
-    await widget.analytics.logRefund(
-      currency: 'USD',
-      value: 123,
-      items: [itemCreator(), itemCreator()],
-    );
-    await widget.analytics.logTutorialBegin();
-    await widget.analytics.logTutorialComplete();
-    await widget.analytics.logUnlockAchievement(id: 'all Firebase API covered');
-    await widget.analytics.logViewItem(
-      currency: 'usd',
-      value: 1000,
-      items: [itemCreator()],
-    );
-    await widget.analytics.logViewItemList(
-      itemListId: 't-shirt-4321',
-      itemListName: 'green t-shirt',
-      items: [itemCreator()],
-    );
-    await widget.analytics.logViewSearchResults(
-      searchTerm: 'test search term',
-    );
-    setMessage('All standard events logged successfully');
-  }
-
+class _ConnectPageState extends State<ConnectPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          MaterialButton(
-            onPressed: _sendAnalyticsEvent,
-            child: const Text('Test logEvent'),
-          ),
-          MaterialButton(
-            onPressed: _testAllEventTypes,
-            child: const Text('Test standard event types'),
-          ),
-          MaterialButton(
-            onPressed: _testSetUserId,
-            child: const Text('Test setUserId'),
-          ),
-          MaterialButton(
-            onPressed: _testSetCurrentScreen,
-            child: const Text('Test setCurrentScreen'),
-          ),
-          MaterialButton(
-            onPressed: _testSetAnalyticsCollectionEnabled,
-            child: const Text('Test setAnalyticsCollectionEnabled'),
-          ),
-          MaterialButton(
-            onPressed: _testSetSessionTimeoutDuration,
-            child: const Text('Test setSessionTimeoutDuration'),
-          ),
-          MaterialButton(
-            onPressed: _testSetUserProperty,
-            child: const Text('Test setUserProperty'),
-          ),
-          MaterialButton(
-            onPressed: _testAppInstanceId,
-            child: const Text('Test appInstanceId'),
-          ),
-          MaterialButton(
-            onPressed: _testResetAnalyticsData,
-            child: const Text('Test resetAnalyticsData'),
-          ),
-          MaterialButton(
-            onPressed: _setDefaultEventParameters,
-            child: const Text('Test setDefaultEventParameters'),
-          ),
-          Text(
-            _message,
-            style: const TextStyle(color: Color.fromARGB(255, 0, 155, 0)),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute<TabsPage>(
-              settings: const RouteSettings(name: TabsPage.routeName),
-              builder: (BuildContext context) {
-                return TabsPage(widget.observer);
-              },
+          Container(
+            alignment: Alignment.center,
+            child: Text(
+              "Bienvenue !",
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: "GoogleSans-Bold",
+                fontSize: 30.53169,
+                fontWeight: FontWeight.w400,
+              ),
+              textAlign: TextAlign.center,
             ),
-          );
-        },
-        child: const Icon(Icons.tab),
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: Text(
+              "Veuillez vous connecter ou \n créer un nouveau compte \n pour utiliser l’application",
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: "ProximaNova-Regular",
+                fontSize: 15.265845,
+                fontWeight: FontWeight.w400,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: "E-mail",
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: "Mot de passe",
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              width: double.infinity,
+              child: RawMaterialButton(
+                fillColor: Color(0xFF636af6),
+                elevation: 0.0,
+                padding: EdgeInsets.symmetric(vertical: 20.0),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(3.52)),
+                onPressed: () {},
+                child: Text("Se connecter",
+                    style: TextStyle(
+                        color: Color(0xFFFFFFff),
+                        fontFamily: "ProximaNova-Regular",
+                        fontSize: 15.239016,
+                        fontWeight: FontWeight.w400)),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              width: double.infinity,
+              child: RawMaterialButton(
+                fillColor: Color(0xFF636af6),
+                elevation: 0.0,
+                padding: EdgeInsets.symmetric(vertical: 20.0),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(3.52)),
+                onPressed: () {},
+                child: Text("Créer un compte",
+                    style: TextStyle(
+                        color: Color(0xFFFFFFff),
+                        fontFamily: "ProximaNova-Regular",
+                        fontSize: 15.239016,
+                        fontWeight: FontWeight.w400)),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: Text(
+              "Mot de passe oublié",
+              style: TextStyle(
+                color: Color(0xAFA9A9A9),
+                fontFamily: "ProximaNova-Regular",
+                fontSize: 15.239016,
+                fontWeight: FontWeight.w400,
+                decoration: TextDecoration.underline,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+
+          /*RawMaterialButton(
+              fillColor: Color(0xFF636af6),
+              elevation: 0.0,
+              padding: EdgeInsets.symmetric(vertical: 20.0),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(3.52)),
+              onPressed: () {},
+              child: Text("Se connecter",
+                  style: TextStyle(
+                    height: 0.2,
+                    color: Colors.white,
+                    fontSize: 15.0,
+                  ))),
+        */
+        ],
       ),
     );
   }
