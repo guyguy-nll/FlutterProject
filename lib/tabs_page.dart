1b806a8929bd2c066a9072279911c9dc92c08636
Guy-Alexandre Novella <73712531+guyguy-nll@users.noreply.github.com>
2023-03-01 13:17:32 +0100

    main1

--- lib/tabs_page.dart
+++ lib/tabs_page.dart
@@ -4,6 +4,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
