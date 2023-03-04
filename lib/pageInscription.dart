b77a260397093a98ff6b07920677840155da7786
Matthieu Gascon <matthieu.gascon@hotmail.fr>
2023-03-04 11:13:06 +0100

    PageInscription

--- lib/pageInscription.dart
+++ lib/pageInscription.dart
@@ -29,7 +29,7 @@ class pageInscription extends StatelessWidget {
           Container(
             alignment: Alignment.center,
             child: Text(
-              "Bienvenue !",
+              "Inscription",
               style: TextStyle(
                 color: Colors.black,
                 fontFamily: "GoogleSans-Bold",
@@ -42,7 +42,7 @@ class pageInscription extends StatelessWidget {
           Container(
             alignment: Alignment.center,
             child: Text(
-              "Page Inscription",
+              "Veuillez saisir ces différentes informations, \nafin que vos listes soient sauvegardées.",
               style: TextStyle(
                 color: Colors.black,
                 fontFamily: "ProximaNova-Regular",
@@ -50,6 +50,16 @@ class pageInscription extends StatelessWidget {
                 fontWeight: FontWeight.w400,
               ),
               textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: "Nom d’utilisateur",
+              ),
             ),
           ),
           Padding(
@@ -72,6 +82,16 @@ class pageInscription extends StatelessWidget {
               ),
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: "Vérification du mot de passe ",
+              ),
+            ),
+          ),
           SizedBox(
             height: 30.0,
           ),
@@ -86,55 +106,13 @@ class pageInscription extends StatelessWidget {
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(3.52)),
                 onPressed: () {},
-                child: Text("Se connecter",
+                child: Text("S'inscrire",
                     style: TextStyle(
                         color: Color(0xFFFFFFff),
                         fontFamily: "ProximaNova-Regular",
                         fontSize: 15.239016,
                         fontWeight: FontWeight.w400)),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Container(
-              width: double.infinity,
-              child: RawMaterialButton(
-                fillColor: Color(0xFF636af6),
-                elevation: 0.0,
-                padding: EdgeInsets.symmetric(vertical: 20.0),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(3.52)),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => pageInscription()),
-                  );
-                },
-                child: Text("Créer un compte",
-                    style: TextStyle(
-                        color: Color(0xFFFFFFff),
-                        fontFamily: "ProximaNova-Regular",
-                        fontSize: 15.239016,
-                        fontWeight: FontWeight.w400)),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 30.0,
-          ),
-          Container(
-            alignment: Alignment.center,
-            child: Text(
-              "Mot de passe oublié",
-              style: TextStyle(
-                color: Color(0xAFA9A9A9),
-                fontFamily: "ProximaNova-Regular",
-                fontSize: 15.239016,
-                fontWeight: FontWeight.w400,
-                decoration: TextDecoration.underline,
-              ),
-              textAlign: TextAlign.center,
             ),
           ),
         ],
