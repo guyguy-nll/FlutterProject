f708ae79cb8f8fab35c71f4f11d9efbfeaba661f
Matthieu Gascon <matthieu.gascon@hotmail.fr>
2023-03-12 16:01:47 +0100

    InscriptionFirebase

--- lib/pageInscription.dart
+++ lib/pageInscription.dart
@@ -15,115 +15,195 @@ import 'tabs_page.dart';
 //source:
 //suivi la video https://www.youtube.com/watch?v=aKgEEnVhU1I&t=61s
 //positionné les éléments: https://medium.flutterdevs.com/stack-and-positioned-widget-in-flutter-3d1a7b30b09a
+//source: https://firebase.google.com/docs/auth/flutter/start?hl=fr
+//https://firebase.google.com/docs/firestore/quickstart?hl=fr
+//https://docs.flutter.dev/
+//pour effectuer l'inscription dans firebase:
+//https://www.freecodespot.com/blog/flutter-login-and-registration-using-firebase/
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:projet/pageAccueil.dart';
 import 'package:projet/searchPage.dart';
 
-class pageInscription extends StatelessWidget {
+class pageInscription extends StatefulWidget {
+  const pageInscription({super.key});
+
+  @override
+  State<pageInscription> createState() => _pageInscription();
+}
+
+class _pageInscription extends State<pageInscription> {
+  final FirebaseAuth Auth = FirebaseAuth.instance;
+  final TextEditingController nomControl = TextEditingController();
+  final TextEditingController mailControl = TextEditingController();
+  final TextEditingController mdpControl = TextEditingController();
+  final TextEditingController mdpverifControl = TextEditingController();
+  bool testmdp = true;
+  bool charge = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            alignment: Alignment.center,
-            child: Text(
-              "Inscription",
-              style: TextStyle(
-                color: Colors.black,
-                fontFamily: "GoogleSans-Bold",
-                fontSize: 30.53169,
-                fontWeight: FontWeight.w400,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            child: Text(
-              "Veuillez saisir ces différentes informations, \nafin que vos listes soient sauvegardées.",
-              style: TextStyle(
-                color: Colors.black,
-                fontFamily: "ProximaNova-Regular",
-                fontSize: 15.265845,
-                fontWeight: FontWeight.w400,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: TextField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                hintText: "Nommm d’utilisateur",
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: TextField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                hintText: "E-mail",
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: TextField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                hintText: "Mot de passe",
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: TextField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                hintText: "Vérification du mot de passe ",
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 30.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Container(
-              width: double.infinity,
-              child: RawMaterialButton(
-                fillColor: Color(0xFF636af6),
-                elevation: 0.0,
-                padding: EdgeInsets.symmetric(vertical: 20.0),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(3.52)),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => pageAccueil()),
-                  );
-                },
-                child: Text("S'inscrire",
+      backgroundColor: Colors.black,
+      body: charge
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Inscription",
                     style: TextStyle(
-                        color: Color(0xFFFFFFff),
-                        fontFamily: "ProximaNova-Regular",
-                        fontSize: 15.239016,
-                        fontWeight: FontWeight.w400)),
-              ),
+                      color: Color(0xFFFFFFff),
+                      fontFamily: "GoogleSans-Bold",
+                      fontSize: 30.53169,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Veuillez saisir ces différentes informations, \nafin que vos listes soient sauvegardées.",
+                    style: TextStyle(
+                      color: Color(0xFFFFFFff),
+                      fontFamily: "ProximaNova-Regular",
+                      fontSize: 15.265845,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextFormField(
+                    controller: nomControl,
+                    style: TextStyle(color: Colors.white),
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey[800],
+                      filled: true,
+                      hintText: "Nom d’utilisateur",
+                      hintStyle: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextFormField(
+                    controller: mailControl,
+                    style: TextStyle(color: Colors.white),
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey[800],
+                      filled: true,
+                      hintText: "E-mail",
+                      hintStyle: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextFormField(
+                    controller: mdpControl,
+                    style: TextStyle(color: Colors.white),
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey[800],
+                      filled: true,
+                      hintText: "Mot de passe",
+                      hintStyle: TextStyle(color: Colors.white),
+                      suffixIcon: testmdp
+                          ? SizedBox()
+                          : Icon(
+                              Icons.warning,
+                              color: Colors.red,
+                            ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextFormField(
+                    controller: mdpverifControl,
+                    style: TextStyle(color: Colors.white),
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey[800],
+                      filled: true,
+                      hintText: "Vérification de mot de passe",
+                      hintStyle: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Container(
+                    width: double.infinity,
+                    child: RawMaterialButton(
+                      fillColor: Color(0xFF636af6),
+                      elevation: 0.0,
+                      padding: EdgeInsets.symmetric(vertical: 20.0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(3.52)),
+                      onPressed: () {
+                        Inscript();
+                      },
+                      child: Text("S'inscrire",
+                          style: TextStyle(
+                              color: Color(0xFFFFFFff),
+                              fontFamily: "ProximaNova-Regular",
+                              fontSize: 15.239016,
+                              fontWeight: FontWeight.w400)),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
     );
+  }
+
+  Future<void> Inscript() async {
+    setState(() {
+      charge = true;
+    });
+    if (mdpControl.text.trim() != mdpverifControl.text.trim()) {
+      setState(() {
+        testmdp = false;
+        charge = false;
+      });
+      return;
+    }
+    try {
+      UserCredential user = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: mailControl.text.trim(), password: mdpControl.text.trim());
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => pageAccueil()),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Erreur: $e'),
+        ),
+      );
+    }
+    setState(() {
+      charge = false;
+    });
   }
 }
